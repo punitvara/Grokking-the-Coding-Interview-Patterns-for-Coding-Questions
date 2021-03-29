1341d91eb12b123c7e952b24f9c78fffbf1f1ff1
Punit Vara <punitvara@gmail.com>
2021-03-29 21:36:41 +0530

    Update Subset Sum (medium).py
    Add DP solution

--- 1. Pattern Sliding Window/Subset Sum (medium).py
+++ 1. Pattern Sliding Window/Subset Sum (medium).py
@@ -15,17 +15,36 @@ Output: False
 The given set does not have any subset whose sum is equal to '6'.
 '''
 
-
 #Recursive Solution
 
 class Solution:
     def isSubsetSum (self, N, arr, sum):
         # code here
-        if (N == 0 and sum == 0):
-            return 1
-        if (N == 0 and sum != 0):
-            return 0
+        if (sum == 0):
+            return True
+        if (N == 0):
+            return False
         if arr[N-1] <= sum:
             return self.isSubsetSum(N-1, arr, sum-arr[N-1]) or self.isSubsetSum(N-1, arr, sum)
         elif arr[N-1] > sum:
             return self.isSubsetSum(N-1, arr, sum)
+
+#Dp solution
+class Solution:
+    def isSubsetSum (self, N, arr, sum):
+        # code here
+
+        dp = [[False for i in range(sum+1)] for j in range(N+1)]
+
+        for j in range(sum+1):
+                dp[0][j] = False
+        for i in range(N+1):
+                dp[i][0] = True
+
+        for i in range(1, N+1):
+            for j in range(1, sum+1):
+                if arr[i-1] <= j:
+                    dp[i][j] =  dp[i-1][j-arr[i-1]] or dp[i-1][j]
+                elif arr[i-1] > sum:
+                    dp[i][j] = dp[i-1][j]
+        return dp[N][sum]
